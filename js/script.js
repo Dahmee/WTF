307cd77e7a2f013593615f9cf2910bde00616d80
Dahmee <82746756+Dahmee@users.noreply.github.com>
2022-09-26 11:34:07 +0100

    Dami

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// let iAmDone = "I am done!";
+// alert(iAmDone);
+// prompt("please enter a password:");smarter
+// var num = 1;
+// var newNum = ++num;
+// console.log(num, newNum);
+// console.log(newNum);
+var x = 2++;
+console.log(x);
